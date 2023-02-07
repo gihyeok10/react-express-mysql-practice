646834ba5a0af930af43943d771eb8da7a91c2f3
kimgihyeok <gh9812301@naver.com>
2023-02-07 15:04:00 +0900

    서버에 post

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import './App.css';
-
+import { useState } from 'react';
 
 //함수형 컴포넌트
 function App() {
-  const selectAll = () => {
-    alert("selectAll!!")
-  }
+ const [id,setId] = useState("")   
+ const handleChange = (e) => {
+    setId(e.target.value)
+    e.target.name = e.target.value
+ }
+
+ const submitId = ()=>{
+    const post = {
+        plzid : id,
+    }
+   
+    fetch("http://localhost:3001/idplz", {
+      method : "post", // 통신방법
+      headers : {
+        "content-type" : "application/json",
+      },
+      body : JSON.stringify(post),
+    });
+  };
+ 
   return (
     <div id="App">
-      <h1>React-Express-MySQL 연결</h1>    
-      <button onClick={selectAll}>모두조회</button>
+             <input onChange={handleChange} name="id"></input>
+             <button onClick={submitId}>Submit</button>
+             <h1>{id}</h1>
     </div>
   );
 }
